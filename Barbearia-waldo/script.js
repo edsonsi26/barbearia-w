5f3954f99be7f94adac79f50a39815d9b77db1b8
edsonsi26 <https://github.com/edsonsi26>
2024-11-23 11:12:06 -0300

    first commit

---
+++ Barbearia-waldo/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const whatsappButton = document.querySelector(".whatsapp-float");
+
+
+    setTimeout(() => {
+        whatsappButton.style.display = "flex";
+    }, 1000);
+});
